5a5f9f2d4ffb1a147e5f0ea9a2b3559b557beaa4
Natallia Vasilyeva <nn1.vasilyeva@gmail.com>
2020-04-19 20:05:32 +0300

    create simple messenger

---
+++ app/src/main/java/com/demo/messenger/ReceiveMessageActivity.java
@@ -0,0 +1,22 @@
+package com.demo.messenger;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class ReceiveMessageActivity extends AppCompatActivity {
+
+    private TextView textViewReceivedMsg;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_receive_message);
+        textViewReceivedMsg=findViewById(R.id.textViewReceivedMsg);
+        Intent intent=getIntent();
+        String msg = intent.getStringExtra("msg");
+        textViewReceivedMsg.setText(msg);
+    }
+}
